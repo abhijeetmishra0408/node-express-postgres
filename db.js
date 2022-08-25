9d7117cb63cda4e813410c1b4f44cae4d9762bea
Abhijeet Mishra <abhijeetmishra0408@gmail.com>
2022-08-25 16:08:41 +0530

    env dev

--- db.js
+++ db.js
@@ -3,23 +3,27 @@ require('dotenv').config();
 
 // const db_url = process.env.DATABASE_URL
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 
-client.connect()
+if (process.env.dev_env) {
+    const client = new Pool({
+        user: process.env.db_user,
+        password: process.env.db_password,
+        host: process.env.db_host,
+        port: process.env.db_port,
+        database: process.env.database
+    })
+    module.exports = client;
+}
 
-// const pool = new Pool({
-//     user: process.env.db_user,
-//     password: process.env.db_password,
-//     host: process.env.db_host,
-//     port: process.env.db_port,
-//     database: process.env.database
-// })
+if (!process.env.dev_env) {
+    const client = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+    module.exports = client;
 
-module.exports = client;
+}
